28d11c4e96bb7793ceb2f6e18b8c8d4b05663ec0
hrushikesh <hrushi74161@gmail.com>
2022-10-29 18:45:39 +0530

    removed sqlite and using postgresql as db

--- blog_application/settings.py
+++ blog_application/settings.py
@@ -25,6 +25,7 @@ DJANGO_BUILTIN_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.sitemaps',
+    'django.contrib.postgres',
 ]
 
 PROJECT_APPS = [
@@ -73,9 +74,17 @@ WSGI_APPLICATION = 'blog_application.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
